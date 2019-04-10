2ba972958adc1df16c6620ee6811d2fe478b504d
Hashim4work <s.hashimali512@gmail.com>
2019-04-10 18:35:15 +0500

    handled deletion of project after deletion of manager

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,5 +7,5 @@ class Project < ApplicationRecord
 
 	has_many :bugs, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5}
-	
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,29 @@
 class User < ApplicationRecord
 	enum user_type: [:developer, :manager, :qa]
 	#has_many :projects, dependent: :destroy
+	before_destroy :check_for_projects
 
 	has_many :bugs, dependent: :destroy
 	has_many :usersprojects, dependent: :destroy
 	has_many :projects, :through => :usersprojects
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+	private
+
+	def check_for_projects
+		if self.manager?
+			projects = Project.all
+			projects.each do |project|
+				if project.users.find_by(id: self.id).present?
+					project.destroy
+				end
+			end
+		end
+	end
 end
